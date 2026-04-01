537603211f53081aafbb97c532e1c8114efd913f
Miguel Ramos <return.infox@gmail.com>
2026-03-31 21:53:36 -0300

    test del modelo

--- productos/tests.py
+++ productos/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from .models import Producto
 # Create your tests here.
+class ProductoModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.producto_base = Producto.objects.create(
+            nombre='Teclado Mecánico',
+            descripcion='Un teclado con luces RGB excelente',
+            precio=120.50,
+            stock=15,
+            categoria='Accesorios',
+            imagen_url='https://example.com/teclado.jpg'
+    )
+
+    def test_producto_str_method(self):
+        # necesito self por que debo utilizar los asserts de TestCase
+        producto = self.producto_base
+        resultado = str(producto)
+        esperado = "Teclado Mecánico (Accesorios)"
+        self.assertEqual(resultado, esperado)
