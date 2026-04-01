76281d72b6ee05230e6738d3793f1281988aef59
Miguel Ramos <return.infox@gmail.com>
2026-04-01 20:30:31 -0300

    validaciones de formulario

--- productos/tests/test_forms.py
+++ productos/tests/test_forms.py
@@ -11,6 +11,28 @@ DATOS_VALIDOS = {
 }
 
 class ProductoFormTest(TestCase):
+
     def test_formulario_valido_con_datos_completos(self):
         form = ProductoForm(data=DATOS_VALIDOS)
         self.assertTrue(form.is_valid(), msg=f"Errores inesperados: {form.errors}")
+
+    def test_formulario_valido_sin_imagen_url(self):
+        datos = DATOS_VALIDOS.copy()
+        datos.pop('imagen_url')  # El campo imagen_url es opcional
+        form = ProductoForm(data=datos)
+        self.assertTrue(form.is_valid(), msg=f"Errores inesperados: {form.errors}")
+
+    def test_nombre_muy_corto_es_invalido(self):
+        datos = {**DATOS_VALIDOS, 'nombre': 'Te'}  # spread operator esparse todas las keys con su valores para copiar y modificar solo el campo 'nombre'
+        form = ProductoForm(data=datos)
+        self.assertFalse(form.is_valid(), msg=f"Errores esperados: {form.errors}")
+        self.assertIn('nombre', form.errors)
+        self.assertIn("El nombre debe tener al menos 3 caracteres reales.", form.errors['nombre'][0])
+
+    
+    def test_nombre_solo_espacios_es_invalido(self):
+        datos = {**DATOS_VALIDOS, 'nombre': '                    '}
+        form = ProductoForm(data=datos)
+        self.assertFalse(form.is_valid(), msg=f"Errores esperados: {form.errors}")
+        self.assertIn('nombre', form.errors)
+        # self.assertIn("El nombre debe tener al menos 3 caracteres reales.", form.errors['nombre'][0])
